ecfc5cdfe6a1d8523e2a5979af2a1df94c2ee628
Bojja <kruparanibojja1730@gmail.com>
2026-01-28 22:51:36 +0530

    completed project successfully

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // or vue/svelte if you’re using those
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/chai-culture-coming-soon1/', // <-- add your repo name here
   plugins: [react()],
-})
+});
+
